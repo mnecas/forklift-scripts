b3a19ea214b834b25af83a64a1180bf64646d324
Martin Nečas <necas.marty@gmail.com>
2023-03-28 20:54:53 +0200

    Use registry proxy
    Signed-off-by: Martin Nečas <necas.marty@gmail.com>

--- get_commits.sh
+++ get_commits.sh
@@ -2,27 +2,22 @@
 
 set -e
 
-echo "Creating registries.conf for brew builds"
-cat << \EOT > ~/.config/containers/registries.conf
-unqualified-search-registries = ["registry.fedoraproject.org", "registry.access.redhat.com", "docker.io", "quay.io"]
-[[registry]]
-location="brew.registry.redhat.io"
-EOT
-
 NAMESPACE=openshift-mtv
 echo "Get API and UI plugin pods"
 API_POD=$(oc get pods -n $NAMESPACE --no-headers=true | awk '/forklift-api/{print $1}')
 UI_POD=$(oc get pods -n $NAMESPACE --no-headers=true | awk '/forklift-ui-plugin/{print $1}')
 
 echo "Get images from pods"
-# The images have use registry.redhat.io address but they are linked inside ocp to brew.registry.redhat.io
-# So we need to add the `brew.` prefix
-API_IMG=brew.$(oc get pods -n $NAMESPACE -o jsonpath="{.spec.containers[*].image}" $API_POD)
-UI_IMG=brew.$(oc get pods -n $NAMESPACE -o jsonpath="{.spec.containers[*].image}" $UI_POD)
+API_IMG=$(oc get pods -n $NAMESPACE -o jsonpath="{.spec.containers[*].image}" $API_POD)
+UI_IMG=$(oc get pods -n $NAMESPACE -o jsonpath="{.spec.containers[*].image}" $UI_POD)
+
+IFS='@' read _ API_IMG_HASH <<< $API_IMG
+IFS='@' read _ UI_IMG_HASH <<< $UI_IMG
+
 
 echo "Get commit from images"
-URL_FORKLIFT=$(skopeo inspect "docker://$API_IMG" -n | jq '.Labels."io.openshift.build.commit.url"' -r)
-URL_FORKLIFT_PLUGIN=$(skopeo inspect "docker://$UI_IMG" -n | jq '.Labels."io.openshift.build.commit.url"' -r)
+URL_FORKLIFT=$(skopeo inspect "docker://registry-proxy.engineering.redhat.com/rh-osbs/migration-toolkit-virtualization-mtv-api-rhel9@$API_IMG_HASH" -n | jq '.Labels."io.openshift.build.commit.url"' -r)
+URL_FORKLIFT_PLUGIN=$(skopeo inspect "docker://registry-proxy.engineering.redhat.com/rh-osbs/migration-toolkit-virtualization-mtv-console-plugin-rhel9@$UI_IMG_HASH" -n | jq '.Labels."io.openshift.build.commit.url"' -r)
 
 echo "----------"
 echo "$URL_FORKLIFT$API_COMMIT"
